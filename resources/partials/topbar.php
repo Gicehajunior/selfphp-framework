bd1cae01cb0afb4e602de1b689e9d817e0630e83
Gicehajunior <gicehajunior76@gmail.com>
2022-01-14 15:53:32 +0300

    revamping

--- resources/partials/topbar.php
+++ resources/partials/topbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-sm navbar-light pt-3 fixed-top">
-    <a class="navbar-brand" href="#"><img src="<?= EnvLoader::env_var()['app_domain'] ?>/public/storage/logo/branded-logo.png" alt="" width="100" height="100"></a>
+    <a class="navbar-brand" href="#"><img src="<?= EnvLoader::env_var()['app_domain'] ?>/public/storage/logo/sp-logo.png" alt="" width="100" height="100"></a>
     <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
