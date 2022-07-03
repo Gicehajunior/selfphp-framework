42ad568260eac33fff8210e7ed7fefd368751cfa
Gicehajunior <gicehajunior76@gmail.com>
2022-07-03 17:17:57 +0300

    bootstrap asset copy to public folder

--- index.php
+++ index.php
@@ -10,7 +10,34 @@ $GLOBALS['controllerPath'] = [
     "./app/http/auth"
 ];
 
-$GLOBALS['modelsPath'] = "./app";
+$GLOBALS['modelsPath'] = "./app"; 
+
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') { 
+    if (!is_dir(__DIR__ . "/public/bootstrap/css") || !is_dir(__DIR__ . "/public/bootstrap/js")) {
+        
+        mkdir(__DIR__ . "/public/bootstrap/css");
+        mkdir(__DIR__ . "/public/bootstrap/js");
+
+        if (is_dir(__DIR__ . "/vendor/twbs/bootstrap/dist/") || is_dir(__DIR__ . "/vendor/twbs/bootstrap/dist/")) {
+            $assets = [glob(__DIR__ . "/vendor/twbs/bootstrap/dist/css/*.css"), glob(__DIR__ . "/vendor/twbs/bootstrap/dist/js/*.js")];
+
+            foreach($assets[0] as $assetKey => $assetValue) {  
+                $assetValueBasename = basename($assetValue);
+                copy($assetValue, __DIR__ . "/public/bootstrap/css/" . $assetValueBasename); 
+            }
+
+            foreach($assets[1] as $assetKey => $assetValue) {
+                $assetValueBasename = basename($assetValue);
+                copy($assetValue, __DIR__ . "/public/bootstrap/js/" . $assetValueBasename); 
+            } 
+        }
+        
+        if (!is_dir(__DIR__ . "/public/bootstrap/css/") || !is_dir(__DIR__ . "/public/bootstrap/js/")) {
+            echo "<span>No Bootstrap Css and Js files set on public!</span>";
+            exit();
+        }
+    }  
+}
 
 $dotenv = Dotenv\Dotenv::createImmutable('./');
 $dotenv->load();
