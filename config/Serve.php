8ac342fb2848d073bd11346036a3bfc95311ac91
Gicehajunior <gicehajunior76@gmail.com>
2023-11-08 04:35:24 +0300

    sql queries improvement

--- config/Serve.php
+++ config/Serve.php
@@ -131,13 +131,13 @@ class Serve
 
             $row_array = array();
             
-            if ($result == true or is_object($result)) {
+            if ($result) {
                 while($rows = mysqli_fetch_assoc($result)){
                     array_push($row_array, $rows);
                 } 
             }
 
-            return $row_array[0];
+            return current($row_array);
         } catch (\Throwable $error) {
             SP::debug_backtrace_show($error); 
         }
