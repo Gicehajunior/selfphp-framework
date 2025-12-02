ae693be2d1eb56d136af8d5ca328ce0be706c9f3
Giceha Junior <gicehajunior76@gmail.com>
2025-12-02 15:29:32 +0300

    chore: add assets config file & update deps for accountability

---
+++ config/Assets.php
@@ -0,0 +1,30 @@
+<?php 
+
+require __DIR__ . '/../vendor/autoload.php';  
+
+return [
+    /**
+     * ------------------------------------------------------------------------
+     *                           S.P Framework v1.0.0
+     * ------------------------------------------------------------------------
+     * Assets settings. Config file for the application assets accessor settings.
+     * ------------------------------------------------------------------------
+     */
+
+    'bootstrap' => [
+
+        // Default version if not defined
+        'default' => '4',
+
+        // Assign version to route groups or named routes
+        'routes' => [
+            // Use Bootstrap 5 for these views/routes
+            '4' => [],
+
+            // Use Bootstrap 5 for these views/routes
+            '5' => [
+                '*'
+            ]
+        ]
+    ]
+];
