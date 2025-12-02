488440ef990d33092ad2e9ce4e2553ec3b8f3405
Giceha Junior <gicehajunior76@gmail.com>
2025-12-02 16:50:48 +0300

    chore: update views to take account of default bootstrap 5 lib

--- resources/auth/register.php
+++ resources/auth/register.php
@@ -15,40 +15,39 @@
                     if (isset($message)) {
                     ?>
                         <div class="alert <?= (($status == 'success') ? 'alert-success' : 'alert-danger') ?> alert-dismissible fade show" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                <span class="sr-only">Close</span>
-                            </button>
                             {{ $message }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div>
                     <?php
                     }
                     ?>
                 </div>
                 <form action="/register" method="post" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="Username">Username</label>
-                        <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username" autofocus autocomplete="on" aria-describedby="helpId" required="required">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username" autofocus autocomplete="on" required>
                     </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" id="email" class="form-control" placeholder="Enter Email" autocomplete="on" aria-describedby="helpId" required="required">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" id="email" class="form-control" placeholder="Enter Email" autocomplete="on" required>
                     </div>
-                    <div class="form-group">
-                        <label for="tel">Contacts</label>
-                        <input type="tel" name="tel" id="tel" class="form-control" placeholder="Enter Contact" autocomplete="on" aria-describedby="helpId" required="required">
+                    <div class="mb-3">
+                        <label for="tel" class="form-label">Contact Number</label>
+                        <input type="tel" name="tel" id="tel" class="form-control" placeholder="Enter Contact Number" autocomplete="on" required>
                     </div>
-                    <div class="form-group">
-                        <label for="Password">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" placeholder="Enter Password" autocomplete="on" aria-describedby="helpId" required="required">
-                        <small id="helpId" class="text-muted">Note: Valid Password</small>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" id="password" class="form-control" placeholder="Enter Password" autocomplete="on" required>
+                        <div class="form-text">Note: Use a strong password</div>
                     </div>
-                    <div class="form-group float-right">
-                        <button type="submit" id="btn-register" class="btn btn-sm btn-primary btn-register">Sign Up</button>
+
+                    <div class="mb-3 text-end">
+                        <button type="submit" id="btn-register" class="btn btn-primary btn-register">Sign Up</button>
                     </div>
-                    <div class="footer-text pt-5">
-                        <p>
-                            Already have an account? Click <a href="/login">here</a>
+                    
+                    <div class="pt-5">
+                        <p class="mb-0">
+                            Already have an account? <a href="/login" class="text-decoration-none">Sign in here</a>
                         </p>
                     </div>
                 </form>
